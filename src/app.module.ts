53c98533ad3c30a0b846c864e784c5188e990b3f
Sajidh Farook <36571307+mfmsajidh@users.noreply.github.com>
2025-03-20 13:14:54 +0000

    refactor: adds scheduling (needed later for emails)

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,13 @@ import applicationConfig from '../config/application.config';
 import validationSchema from '../config/validation.config';
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ScheduleModule } from '@nestjs/schedule';
 
 const ENV_PATHS = ['.env.development.local', '.env.test.local', '.env.production.local', '.env.local', '.env'];
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       envFilePath: ENV_PATHS,
       load: [applicationConfig, swaggerConfig],
